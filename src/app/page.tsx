2dc58b157ccdf092dcf35a9f086823d5b11c85c0
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2024-12-10 18:23:10 +0100

    add wallet client, update permission logic, structure code and ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { LocalAccountSigner } from '@aa-sdk/core';
-import { alchemy, sepolia } from '@account-kit/infra';
+import { WalletClientSigner } from '@aa-sdk/core';
+import { alchemy, AlchemySmartAccountClient, sepolia } from '@account-kit/infra';
 import {
+  IPluginAbi,
   SessionKeyAccessListType,
   SessionKeyPermissionsBuilder,
   SessionKeyPlugin,
@@ -10,119 +11,160 @@ import {
   createModularAccountAlchemyClient,
   sessionKeyPluginActions,
 } from '@account-kit/smart-contracts';
-import { encodeFunctionData, Hex, zeroHash } from 'viem';
+import { useEffect, useState } from 'react';
+import { createWalletClient, custom, encodeFunctionData, zeroHash } from 'viem';
 import styles from './page.module.css';
 import {
   abi as registryAbi,
   address as registryAddress,
 } from './RegistryContract';
-import { useEffect, useState } from 'react';
+import { writeContract } from 'viem/actions';
 
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY as string;
-console.log('process.env.API_KEY', process.env.NEXT_PUBLIC_API_KEY);
-const PRIVATE_KEY = process.env.NEXT_PUBLIC_PRIVATE_KEY as Hex;
+const chain = sepolia;
 
 export default function Home() {
-  const [client, setClient] = useState<any>();
-  const [isLoading, setLoading]= useState<boolean>(false);
+  const [client, setClient] = useState<AlchemySmartAccountClient | undefined>();
+  const [sessionKeyClient, setSessionKeyClient] = useState<any>();
+  const [isConnected, setIsConnected] = useState(false);
+  const [isLoading, setLoading] = useState<boolean>(false);
   const [sessionKeySigner, setSessionKeySigner] = useState<SessionKeySigner>();
   const [countValue, setCountValue] = useState<number>();
-  const chain = sepolia;
   const transport = alchemy({ apiKey: API_KEY });
-  const signer = LocalAccountSigner.privateKeyToAccountSigner(PRIVATE_KEY);
+
+  const walletClient = createWalletClient({
+    chain: sepolia,
+    transport: custom((window as any).ethereum),
+  });
+
+  const signer = new WalletClientSigner(walletClient, 'wallet');
 
   useEffect(() => {
+    // check connection status on page load
     (async () => {
-      console.log('signer', signer);
-      const sessionKeySigner = new SessionKeySigner();
-      setSessionKeySigner(sessionKeySigner);
-      const client = (
-        await createModularAccountAlchemyClient({
-          chain,
-          transport,
-          signer,
-        })
-      ).extend(sessionKeyPluginActions);
-      setClient(client);
+      const accounts = await walletClient.request({
+        method: 'eth_accounts',
+      });
+
+      if (accounts.length > 0) {
+        setIsConnected(true);
+        await initializeClient();
+      }
     })();
   }, []);
 
-  async function readCount() {
+  const connectWallet = async () => {
     try {
-      const countValue = await client.readContract({
-        address: registryAddress,
-        abi: registryAbi,
-        functionName: 'count',
-        args: [client.getAddress({ account: client.account })],
+      const accounts = await walletClient.request({
+        method: 'eth_requestAccounts',
       });
-      setCountValue(countValue);
-
-      console.log('Count value:', countValue);
+      if (accounts.length > 0) {
+        setIsConnected(true);
+        await initializeClient();
+        console.log('Connected account:', accounts[0]);
+      } else {
+        console.log('No accounts found.');
+      }
     } catch (error) {
-      console.error('Error reading count:', error);
+      console.error('Error connecting to wallet:', error);
     }
-  }
+  };
 
-  async function commitThis() {
-    setLoading(true);
-    if (!sessionKeySigner) return;
-    const sessionKeyAddress = await sessionKeySigner.getAddress();
-    // 1. check if the plugin is installed
+  const initializeClient = async () => {
+    const sessionKeySigner = new SessionKeySigner();
+    setSessionKeySigner(sessionKeySigner);
+
+    const client: any = (
+      await createModularAccountAlchemyClient({
+        chain,
+        transport,
+        signer,
+      })
+    ).extend(sessionKeyPluginActions);
+    setClient(client);
+
     const isPluginInstalled = await client
       .getInstalledPlugins({})
-      // This checks using the default address for the chain, but you can always pass in your own plugin address here as an override
-      .then((x: any) => x.includes(SessionKeyPlugin.meta.addresses[chain.id]));
+      .then((x: any) => {
+        console.log(x);
+        return x.includes(SessionKeyPlugin.meta.addresses[chain.id])
+      });
 
-    // 2. if the plugin is not installed, then install it and set up the session key
+
+    const sessionKeyAddress = await sessionKeySigner.getAddress();
+    console.log("isPluginInstalled", isPluginInstalled);
+
     if (!isPluginInstalled) {
-      // lets create an initial permission set for the session key giving it an eth spend limit
       const initialPermissions = new SessionKeyPermissionsBuilder()
-        .setNativeTokenSpendLimit({
-          spendLimit: 1n,
-        })
-        // this will allow the session key plugin to interact with all addresses
+        .setNativeTokenSpendLimit({ spendLimit: 1n })
         .setContractAccessControlType(SessionKeyAccessListType.ALLOW_ALL_ACCESS)
         .setTimeRange({
           validFrom: Math.round(Date.now() / 1000),
-          // valid for 1 hour
           validUntil: Math.round(Date.now() / 1000 + 60 * 60),
         });
-      console.log('we are here initialPermissions');
-
-      console.log(
-        'await sessionKeySigner.getAddress()',
-        await sessionKeySigner.getAddress()
-      );
 
       const { hash } = await client.installSessionKeyPlugin({
-        // 1st arg is the initial set of session keys
-        // 2nd arg is the tags for the session keys
-        // 3rd arg is the initial set of permissions
         args: [[sessionKeyAddress], [zeroHash], [initialPermissions.encode()]],
       });
 
-      console.log('we are here installSessionKeyPlugin');
-
       await client.waitForUserOperationTransaction({ hash });
+    } else {
+      const result = await client.updateSessionKeyPermissions({
+        key: sessionKeyAddress,
+        permissions: new SessionKeyPermissionsBuilder()
+          .setNativeTokenSpendLimit({ spendLimit: 1n })
+          .setContractAccessControlType(SessionKeyAccessListType.ALLOW_ALL_ACCESS)
+          .setTimeRange({
+            validFrom: Math.round(Date.now() / 1000),
+            // valid for 1 hour
+            validUntil: Math.round(Date.now() / 1000 + 60 * 60),
+          })
+          .encode(),
+      });
+      // const result = await client.removeSessionKey({
+      //   key: sessionKeyAddress,
+      // });
+      console.log("update result", result);
     }
 
-    // 3. set up a client that's using our session key
     const sessionKeyClient = (
       await createModularAccountAlchemyClient({
         chain,
         signer: sessionKeySigner,
         transport,
-        // this is important because it tells the client to use our previously deployed account
-        accountAddress: client.getAddress({ account: client.account }),
+        accountAddress: client.getAddress({ account: client.account! }),
       })
     ).extend(sessionKeyPluginActions);
+
+    setSessionKeyClient(sessionKeyClient);
+  };
+
+  const readCount = async () => {
+    if (!client) return;
+    try {
+      const countValue = await client.readContract({
+        address: registryAddress,
+        abi: registryAbi,
+        functionName: 'count',
+        args: [client.getAddress({ account: client.account! })],
+      });
+      setCountValue(countValue as number);
+      console.log('Count value:', countValue);
+    } catch (error) {
+      console.error('Error reading count:', error);
+    }
+  };
+
+  const commitThis = async () => {
+    if (!sessionKeySigner || !sessionKeyClient) return;
+    setLoading(true);
 
     const data = encodeFunctionData({
       abi: registryAbi,
       functionName: 'increament1',
     });
 
-    // 4. send a user operation using the session key
+    const sessionKeyAddress = await sessionKeySigner.getAddress();
     const result = await sessionKeyClient.executeWithSessionKey({
       args: [
         [
@@ -135,32 +177,22 @@ export default function Home() {
         sessionKeyAddress,
       ],
     });
-    console.log(result);
 
+    console.log(result);
     setLoading(false);
     readCount();
-  }
+  };
 
-  async function revealThis() {
+  const revealThis = async () => {
+    if (!sessionKeySigner || !sessionKeyClient) return;
     setLoading(true);
-    if (!sessionKeySigner) return;
-    const sessionKeyAddress = await sessionKeySigner.getAddress();
-    const sessionKeyClient = (
-      await createModularAccountAlchemyClient({
-        chain,
-        signer: sessionKeySigner,
-        transport,
-        // this is important because it tells the client to use our previously deployed account
-        accountAddress: client.getAddress({ account: client.account }),
-      })
-    ).extend(sessionKeyPluginActions);
 
     const data = encodeFunctionData({
       abi: registryAbi,
       functionName: 'increament2',
     });
 
-    // 4. send a user operation using the session key
+    const sessionKeyAddress = await sessionKeySigner.getAddress();
     const result = await sessionKeyClient.executeWithSessionKey({
       args: [
         [
@@ -173,19 +205,25 @@ export default function Home() {
         sessionKeyAddress,
       ],
     });
-    console.log(result);
 
+    console.log(result);
     setLoading(false);
     readCount();
-  }
+  };
+
   return (
     <div className={styles.page}>
-      <div></div>
-      <div>{countValue}</div>
-      <button onClick={readCount}>Refresh Count</button>
-      <div>{isLoading ? 'Waiting tx to be mined...' : '' }</div>
-      <button onClick={commitThis}>Commit this with Signature</button>
-      <button onClick={revealThis}>Reveal this with Signature</button>
+      {!isConnected ? (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      ) : (
+        <>
+          <div>{countValue}</div>
+          <button onClick={readCount}>Refresh Count</button>
+          <div>{isLoading ? 'Waiting tx to be mined...' : ''}</div>
+          <button onClick={commitThis}>Commit this with Signature</button>
+          <button onClick={revealThis}>Reveal this with Signature</button>
+        </>
+      )}
     </div>
   );
 }
